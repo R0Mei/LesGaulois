3802355473adb3ae4d38931d8012bd675922b685
R0Mei <romain.pezet@univ-tlse3.fr>
2024-01-19 08:29:02 +0100

    diapo7

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author PZR4499A
+ *
+ */
+module LesGaulois {
+}
---
+++ src/personnages/gaulois.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class gaulois {
+	private int force;
+	private String name; 
+	public gaulois(int force, String name) 
+	{
+		this.force = force;
+		this.name = name;
+	}
+	public String getGaulois()
+	{
+		return(name);
+	}
+}
